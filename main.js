d7ac304a169a9d86272a987cba0f1e06e984f0a8
Bunhong3 <hongnacap2022@gmail.com>
2025-07-15 20:12:59 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('userForm');
+    const userInfo = document.getElementById('userInfo');
+    const displayName = document.getElementById('displayName');
+    const displayScore = document.getElementById('displayScore');
+    const score = document.getElementById('score');
+    let grade;
+    
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      const name = document.getElementById('name').value;
+      const score = document.getElementById('score').value;
+    if(score>=90){
+        grade = 'A. You passed 🎉 the test';
+    }else if(score>=80){
+        grade = 'B. You passed 🎉 the test';
+    }else if(score>=70){
+        grade = 'C. You passed 🎉 the test';
+    }else if(score>=60){
+        grade = 'D. You passed 🎉 the test';
+    }else if(score>=50){
+        grade = 'E. You passed 🎉 the test';
+    }
+    else{
+        grade = 'F. You failed 😢 the test';
+    }
+      displayName.textContent = name;
+      displayScore.textContent = grade;
+      userInfo.style.display = 'block';
+    });
